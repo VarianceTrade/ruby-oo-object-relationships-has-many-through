c6b6e0d23470d9df3504b427fcadaac2e4f99311
Brandon <sugb73@gmail.com>
2021-03-07 19:59:42 -0500

    commit tree 8e9d70940a7a80cd8a977d3a5f3e79696bbca76e

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,51 @@
 class Waiter
+    
+    @@all = []
 
+    def initialize(name, experience)
+        @name = name
+        @experience = experience
+        @@all << self
+
+    end
+
+    def name
+        @name
+    end
+
+    def experience
+        @experience
+    end
+
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        Meal.new(self, customer, total, tip)
+    end
+
+    def meals
+        meals = []
+        Meal.all.each do |meal|
+            if meal.waiter == self
+                meals << meal
+            end
+        end
+        return meals
+    end
+
+    def best_tipper
+        largest_tip = 0
+        found = nil
+
+        Meal.all.each do |meal|
+            if meal.tip > largest_tip
+                found = meal
+                largest_tip = meal.tip
+            end
+        end
+
+        return found.customer
+    end
 end
